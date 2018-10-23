c092cf234173c5ec4d37407d750b17354525a799
allan-bonadio <orgmgr@tactileint.org>
2018-10-22 23:02:02 -0700

    to deploy on tactileint.org

---
+++ upload.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+# upload the build directory to my website
+# sorry I couldn't get recursive to work with scp or sftp
+sftp allan@ojibwe << APPLETREE
+
+cd /usr/local/nginx/ant-analyze
+pwd
+lcd build
+lpwd
+
+put *.*
+cd static/css
+lcd static/css
+put *.*
+cd ../js
+lcd ../js
+put *.*
+
+APPLETREE
+
+exit
+
+# put -r build/* /usr/local/nginx/ant-analyze
+# 
+# 
+# put build/*.* /usr/local/nginx/ant-analyze
+# put -R build/static /usr/local/nginx/ant-analyze
+# 
+# 
+# cd /usr/local/nginx/ant-analyze
+# put -R build
+# put -r build/static/*
+# 
+
+
+
+
+
+
+
