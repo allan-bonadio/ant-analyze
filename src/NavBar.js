950b6176e26026f0d26625b2c1d5f7466d7f2751
allan-bonadio <orgmgr@tactileint.org>
2018-10-22 18:39:42 -0700

    labels now are formulas

--- src/NavBar.js
+++ src/NavBar.js
@@ -16,16 +16,10 @@ class NavBar extends Component {
 	render() {
 		let buttons = config.scenes.map((scene, ix) => {
 			let sel = (this.props.selectedIndex === ix) ? 'selected' : '';
-			return <button type='button' key={ix} className={sel} onClick={this.clickNumber} >
-				{ix + 1}
+			return <button type='button' key={ix} index={ix} className={sel} onClick={this.clickNumber} >
+				{scene.formula}
 			</button>;
 		});
-////		for (let step = 0; step < this.props.nSteps; step++) {
-////			let cls = (step == this.props.selectedIndex) ? 'selected' : '';
-////			buttons.push(<button type='button' className={cls} onClick={this.clickNumber} >
-////				{step + 1}
-////			</button>);
-////		}
 
 		// the next button
 		buttons.push(<button type='button' key='next' onClick={this.clickNext} >
@@ -41,8 +35,7 @@ class NavBar extends Component {
 	
 	// human clicks on one of the number buttons
 	clickNumber(ev) {
-		let stepIndex = +ev.target.innerText - 1;
-		StepWidget.goToStep(stepIndex);
+		StepWidget.goToStep(+ev.target.getAttribute('key'));
 	}
 	
 	// click on the NEXT> button
