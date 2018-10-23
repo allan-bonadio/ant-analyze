24076184b98cb0302194e5dff096dd47b6935ab4
allan-bonadio <orgmgr@tactileint.org>
2018-10-22 18:47:44 -0700

    added 'formula' field to each scene for new label

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ export let config = {
 	scenes: [
 		{
 			title: "Simple Sine Wave",
+			formula: "sin(x)",
 			blurb: "The familiar Sine function crosses zero at integer multiples of π.  The angle is measured in radians, which is the angle you get when you crawl 1 radius length along the outside edge of a circle.",
 			func: x => Math.sin(x),
 			nPoints: 100,
@@ -12,6 +13,7 @@ export let config = {
 		},
 		{
 			title: "sin(x) / x",
+			formula: "sin(x) / x",
 			blurb: "This is one of the most popular functions for graphing.  It actually has a singularity at x = 0, but it's a removable singularity.  The limit as x goes to zero is 1 because sin(x) is roughly equal to x near x = 0 .  As long as the graph doesn't try to evaluate at exactly zero, it'll look fine.",
 			func: x => Math.sin(x) / x,
 			nPoints: 200,  // must be even to avoid singularity
@@ -20,6 +22,7 @@ export let config = {
 		},
 		{
 			title: "Sine of Reciprocal",
+			formula: "sin(1/x)",
 			blurb: "Because the reciprocal goes to infinity as you approach zero, the sine wave fluctuates wildly.  There is an essential singularity at x = 0; the value at zero is ambiguous, anywhere over -1 ... 1, and even beyond, if you approach from different angles over the complex plane.  The graph is almost impossible to draw near zero for that reason.",
 			func: x => Math.sin(1/x),
 			nPoints: 1000,  // needs more cuz of the tight curves
