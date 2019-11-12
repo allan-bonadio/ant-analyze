d9b3522bec48616078701ab8ca2f4dd7321d1a4f
Allan Bonadio <bojnac@tactileint.org>
2019-11-12 14:12:04 -0800

    added styles of blurb box and nav bar ; added changes for touch events

--- src/BlurbBox.js
+++ src/BlurbBox.js
@@ -3,11 +3,38 @@ import React, { Component } from 'react';
 import config from './config';
 import NavBar from './NavBar';
 
+import './BlurbBox.css';
+
 class BlurbBox extends Component {
+	// the calculated dimensions of the svg initially estimates this box to be some fixed height.
+	// once we've rendered once
+	componentDidMount() {
+	
+	}
+
 	render() {
 		let scene = config.scenes[this.props.selectedIndex];
+////		let touchDisplay ='block';
+////		let mouseDisplay = 'none';
+////		if (! ('ontouchstart' in document.body)) {
+////			touchDisplay = 'none';
+////			mouseDisplay = 'block';
+////		}
+////		let touchDisplay =  ? 'block' : 'none';
+////		let touchDisplay = 'ontouchstart' in document.body ? 'block' : 'none';
+		let touchDisplay = 'ontouchstart' in document.body ? 'block' : 'none';
+		let mouseDisplay = 'ontouchstart' in document.body ? 'none' : 'block';
+		
 		return (
 			<div className='blurb-box' >
+				<p className='desktop-instructions' style={{display: mouseDisplay}}>
+					Click and drag to scroll sideways or vertically.
+				</p>
+				<p className='mobile-instructions' style={{display: touchDisplay}}>
+					Drag to scroll.  Two fingers zoom in/out.
+					<br/><big> ↔ </big> zoom along <i>x</i> axis
+					<br/><big> ↕ </big> zoom along <i>y</i> axis
+				</p>
 				<NavBar nSteps={config.scenes.length} selectedIndex={this.props.selectedIndex} />
 				<h2 className='blurb-title'>
 					{scene.title}
@@ -15,10 +42,10 @@ class BlurbBox extends Component {
 				<p className='blurb-text'>
 					{scene.blurb}
 				</p>
+				<br clear='right' />
 			</div>
 		);
 	}
-
 
 }
 
