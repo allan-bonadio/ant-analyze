083f5e57d7ae571cafb99fe3991c28f509b6b3fb
Allan Bonadio <bojnac@tactileint.org>
2019-12-16 22:25:12 -0800

    - got rid of grid that wasn't doing anything - added 3D-instructions; overhauled mechanism

--- src/BlurbBox.js
+++ src/BlurbBox.js
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq, no-throw-literal  */
 import React, { Component } from 'react';
 
 import config from './config';
@@ -14,19 +15,21 @@ class BlurbBox extends Component {
 
 	render() {
 		let scene = config.scenes[this.props.requestedIndex];
-		let touchDisplay = 'ontouchstart' in document.body ? 'block' : 'none';
-		let mouseDisplay = 'ontouchstart' in document.body ? 'none' : 'block';
+		
+		// decide which instructions to display
+		let touchDisplay = 'none', mouseDisplay = 'none', threeDDisplay = 'none';
+		if (scene.graphics == '2D') {
+			if ('ontouchstart' in document.body)
+				touchDisplay = 'block';
+			else
+				mouseDisplay = 'block';
+		}
+		else
+			threeDDisplay = 'block';
+		
 		
 		return (
 			<div className='blurb-box' >
-				<p className='desktop-instructions' style={{display: mouseDisplay}}>
-					Click and drag to scroll sideways or vertically.
-				</p>
-				<p className='mobile-instructions' style={{display: touchDisplay}}>
-					Drag to scroll.  Two fingers zoom in/out.
-					<br/><big> ↔ </big> zoom along <i>x</i> axis
-					<br/><big> ↕ </big> zoom along <i>y</i> axis
-				</p>
 				<NavBar nSteps={config.scenes.length} 
 						requestedIndex={this.props.requestedIndex} />
 				<h2 className='blurb-title' 
@@ -36,6 +39,17 @@ class BlurbBox extends Component {
 					{scene.blurb}
 				</p>
 				<br clear='right' />
+				<p className='desktop-instructions' style={{display: mouseDisplay}}>
+					Click and drag to scroll sideways or vertically.
+				</p>
+				<p className='mobile-instructions' style={{display: touchDisplay}}>
+					Drag to scroll.  Two fingers zoom in/out.
+					<br/><big> ↔ </big> zoom along <i>x</i> axis
+					<br/><big> ↕ </big> zoom along <i>y</i> axis
+				</p>
+				<p className='3D-instructions' style={{display: threeDDisplay}}>
+					Drag to rotate.
+				</p>
 			</div>
 		);
 	}
