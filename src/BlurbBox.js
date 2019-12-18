9f5f80c152a2b5beec754da6c7b73db31caefe51
Allan Bonadio <bojnac@tactileint.org>
2019-12-18 08:59:10 -0800

    now a functional component

--- src/BlurbBox.js
+++ src/BlurbBox.js
@@ -1,63 +1,50 @@
 /* eslint-disable eqeqeq, no-throw-literal  */
-import React, { Component } from 'react';
+import React from 'react';
 
 import config from './config';
 import NavBar from './NavBar';
 
 import './BlurbBox.css';
 
-class BlurbBox extends Component {
-	constructor(props) {
-		super(props);
-		
-		this.state = {menuShows: false};
-	}
-	// the calculated dimensions of the svg initially estimates this box to be some fixed height.
-	// once we've rendered once
-	componentDidMount() {
+function BlurbBox(props) {
+	let scene = config.scenes[props.requestedIndex];
 	
-	}
-
-	render() {
-		let scene = config.scenes[this.props.requestedIndex];
-		
-		// decide which instructions to display
-		let touchDisplay = 'none', mouseDisplay = 'none', threeDDisplay = 'none';
-		if (scene.graphics == '2D') {
-			if ('ontouchstart' in document.body)
-				touchDisplay = 'block';
-			else
-				mouseDisplay = 'block';
-		}
+	// decide which instructions to display
+	let touchDisplay = 'none', mouseDisplay = 'none', threeDDisplay = 'none';
+	if (scene.graphics == '2D') {
+		if ('ontouchstart' in document.body)
+			touchDisplay = 'block';
 		else
-			threeDDisplay = 'block';
-		
-		
-		return (
-			<div id='blurb-box' style={this.props.style} >
-				<NavBar nSteps={config.scenes.length} 
-						requestedIndex={this.props.requestedIndex} />
-				<h2 className='blurb-title' 
-						dangerouslySetInnerHTML={{__html: scene.title}}>
-				</h2>
-				<p className='blurb-text'>
-					{scene.blurb}
-				</p>
-				<br clear='right' />
-				<p className='desktop-instructions' style={{display: mouseDisplay}}>
-					Click and drag to scroll sideways or vertically.
-				</p>
-				<p className='mobile-instructions' style={{display: touchDisplay}}>
-					Drag to scroll.  Two fingers zoom in/out.
-					<br/><big> ↔ </big> zoom along <i>x</i> axis
-					<br/><big> ↕ </big> zoom along <i>y</i> axis
-				</p>
-				<p className='3D-instructions' style={{display: threeDDisplay}}>
-					Drag to rotate.
-				</p>
-			</div>
-		);
+			mouseDisplay = 'block';
 	}
+	else
+		threeDDisplay = 'block';
+	
+	
+	return (
+		<div id='blurb-box' style={props.style} >
+			<NavBar nSteps={config.scenes.length} 
+					requestedIndex={props.requestedIndex} />
+			<h2 className='blurb-title' 
+					dangerouslySetInnerHTML={{__html: scene.title}}>
+			</h2>
+			<p className='blurb-text'>
+				{scene.blurb}
+			</p>
+			<br clear='right' />
+			<p className='desktop-instructions' style={{display: mouseDisplay}}>
+				Click and drag to scroll sideways or vertically.
+			</p>
+			<p className='mobile-instructions' style={{display: touchDisplay}}>
+				Drag to scroll.  Two fingers zoom in/out.
+				<br/><big> ↔ </big> zoom along <i>x</i> axis
+				<br/><big> ↕ </big> zoom along <i>y</i> axis
+			</p>
+			<p className='3D-instructions' style={{display: threeDDisplay}}>
+				Drag to rotate.
+			</p>
+		</div>
+	);
 
 }
 
